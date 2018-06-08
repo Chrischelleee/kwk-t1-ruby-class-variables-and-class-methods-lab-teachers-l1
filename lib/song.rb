e9ff25b74907d3e33508f9bc928a1233a572a59e
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-08 02:53:18 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def name
+    @name
+  end
+
+  def artist
+    @artist
+  end
+
+  def genre
+    @genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_count = Hash.new(0)
+    @@genres.each do |gen|
+      genre_count[gen] += 1
+    end
+    return genre_count
+  end
+
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each do |art|
+      artist_count[art] += 1
+    end
+    return artist_count
+  end
+end
