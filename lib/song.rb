5533d7689406760cb0310570a93e1ab6e6b2204c
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-08 02:53:45 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -27,13 +27,15 @@ class Song
     @@count
   end
 
+  def self.artists
+    @@artists.uniq
+  end
+  
   def self.genres
     @@genres.uniq
   end
 
-  def self.artists
-    @@artists.uniq
-  end
+
 
   def self.genre_count
     genre_count = Hash.new(0)
